8bc479f2aab09fc110824130d4379344d9610e8d
Peter Joh <johpeter@gmail.com>
2023-02-20 00:56:10 -0700

    fixing schema

--- app/models/dev_skills.py
+++ app/models/dev_skills.py
@@ -2,6 +2,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 devskills = db.Table(
     'devskills',
-    db.Column('developerId', db.Integer, db.ForeignKey('developers.id', ondelete='CASCADE'), primary_key=True, nullable=False),
-    db.Column('skillId', db.Integer, db.ForeignKey('skills.id'), primary_key=True, nullable=False)
+    db.Column('developerId', db.Integer, db.ForeignKey(add_prefix_for_prod('developers.id', ondelete='CASCADE')), primary_key=True, nullable=False),
+    db.Column('skillId', db.Integer, db.ForeignKey(add_prefix_for_prod('skills.id')), primary_key=True, nullable=False)
 )
+
+if environment == "production":
+    devskills.schema = SCHEMA
--- migrations/env.py
+++ migrations/env.py
@@ -3,13 +3,15 @@ from __future__ import with_statement
 import logging
 from logging.config import fileConfig
 
-from flask import current_app
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
 
 import os
 environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get('SCHEMA')
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,10 +26,10 @@ logger = logging.getLogger('alembic.env')
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from flask import current_app
 config.set_main_option(
     'sqlalchemy.url',
-    str(current_app.extensions['migrate'].db.get_engine().url).replace(
-        '%', '%%'))
+    str(current_app.extensions['migrate'].db.engine.url).replace('%', '%%'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -38,15 +40,12 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -59,17 +58,13 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
     # when there are no changes to the schema
     # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
-def run_migrations_online():
-
     def process_revision_directives(context, revision, directives):
         if getattr(config.cmd_opts, 'autogenerate', False):
             script = directives[0]
@@ -90,7 +85,6 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
-
         # Create a schema (only in production)
         if environment == "production":
             connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
@@ -101,7 +95,6 @@ def run_migrations_online():
                 context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
-# keep logic at bottom of file to run migration functions
 if context.is_offline_mode():
     run_migrations_offline()
 else:
