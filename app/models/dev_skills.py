23b2e99acb202a123f6a260449bdce8f5ee65322
Peter Joh <johpeter@gmail.com>
2023-02-20 01:08:55 -0700

    fixed some syntax bugs

--- app/models/dev_skills.py
+++ app/models/dev_skills.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 devskills = db.Table(
     'devskills',
-    db.Column('developerId', db.Integer, db.ForeignKey(add_prefix_for_prod('developers.id', ondelete='CASCADE')), primary_key=True, nullable=False),
+    db.Column('developerId', db.Integer, db.ForeignKey(add_prefix_for_prod('developers.id')), ondelete='CASCADE', primary_key=True, nullable=False),
     db.Column('skillId', db.Integer, db.ForeignKey(add_prefix_for_prod('skills.id')), primary_key=True, nullable=False)
 )
 
